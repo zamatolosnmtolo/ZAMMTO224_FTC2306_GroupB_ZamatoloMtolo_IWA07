b8b41e10ffaec7184ad835b7663401b3b3f978d3
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-08-15 23:06:44 +0200

    Update script

--- Challenge 3/scripts.js
+++ Challenge 3/scripts.js
@@ -1,29 +1,17 @@
-const leoName = 'Leo'
-const leoSurname = 'Musvaire     '
-const leoBalance = '-9394'
+const leoName = 'Leo';
+const leoSurname = 'Musvaire     ';
+const leoBalance = -9394;
 
-const sarahName = 'Sarah    '
-const sarahSurname = 'Kleinhans'
-const sarahBalance = '-4582.2'
+const sarahName = 'Sarah    ';
+const sarahSurname = 'Kleinhans';
+const sarahBalance = -4582.2;
 
-const divider = '----------------------------------'
+const divider = '----------------------------------';
 
-// Only change below this line
+const owed = (parseFloat(leoBalance) + parseFloat(sarahBalance)).toFixed(2); // Calculate owed amount
+const leo = `${leoName} ${leoSurname.trim()} (Owed: R ${Math.abs(parseFloat(leoBalance)).toFixed(2)})`;
+const sarah = `${sarahName.trim()} ${sarahSurname} (Owed: R ${Math.abs(parseFloat(sarahBalance)).toFixed(2)})`;
+const total = `Total amount owed: R ${Math.abs(owed).toFixed(2)}`;
+const result = `${leo}\n${sarah}\n\n${divider}\n${total}\n${divider}`;
 
-// Calculate the total owed amount
-const owed = leoBalance + sarahBalance;
-
-// Format Leo's message
-const leoMessage = `${leoName} ${leoSurname} Owed: R ${Math.abs(leoBalance).toFixed(2)}`;
-
-// Format Sarah's message
-const sarahMessage = `${sarahName} ${sarahSurname} Owed: R ${Math.abs(sarahBalance).toFixed(2)}`;
-
-// Format the total owed message
-const totalOwedMessage = `Total amount owed: R ${Math.abs(owed).toFixed(2)}`;
-
-// Construct the final result
-const result = `\n${leoMessage}\n${sarahMessage}\n\n${divider}\n${totalOwedMessage}\n${divider}`;
-
-// Print the result
 console.log(result);
